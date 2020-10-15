7c9f354bafe3b8eb1b2cf2dbd09aad24c44e7552
Saleh Alibrahim <s.albrahim@outlook.com>
2020-10-16 02:26:23 +0300

    fix: fix issue where you can press the filter button multi time with the same result

--- background.js
+++ background.js
@@ -2,7 +2,8 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.getSelected(null, function (tab) {
         let url = tab.url;
-        if (url.includes('twitter.com/search?q=')) {
+        if (url.includes('twitter.com/search?q=') &&
+            !url.includes('-filter%3Alinks%20')) {
 
             url = url.replace('search?q=', 'search?q=-filter%3Alinks%20')
 
