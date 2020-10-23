634524423dbb2fe40fba5ab4553832f18f5f4460
unknown <s.albrahim@outlook.com>
2020-10-23 14:43:43 +0300

    chore: Update the icon project

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.getSelected(null, function (tab) {
         let url = tab.url;
         if (url.includes('twitter.com/search?q=') &&
-            !url.includes('-filter%3Alinks%20')) {
+            !url.includes('-filter%3Alinks%20')){
             url = url.replace('search?q=', 'search?q=-filter%3Alinks%20')
             chrome.tabs.update(tab.id, { url });
         }
